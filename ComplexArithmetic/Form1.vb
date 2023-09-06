63e69b411a1492014c8ee98ed07e3831dca94cf2
nomvula <nzshabangu95@gmail.com>
2023-09-06 11:46:24 +0200

    Add project files.

---
+++ ComplexArithmetic/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim Price As Decimal
+        Dim Quantity As Decimal
+        Dim Discount As Decimal
+        Dim VAT As Decimal
+        Dim TotalPrice As Decimal
+        Dim TotalVAT As Decimal
+
+
+        Price = 10
+        Discount = 2
+        Quantity = 10
+        VAT = 0.15
+        TotalPrice = ((Price - Discount) * Quantity)
+        MsgBox("The total price without VAT is " & TotalPrice)
+
+        TotalVAT = ((Price - Discount) * Quantity) * VAT + ((Price - Discount) * Quantity)
+        MsgBox("The total price with VAT is " & TotalVAT)
+    End Sub
+End Class
